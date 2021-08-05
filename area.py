7c657036f0847336e6e4144c5a5aa3ed17ac53b7
sohil672007 <71943811+sohil672007@users.noreply.github.com>
2021-08-05 10:49:39 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+
+width = float(input('Please Enter the Width of a Rectangle: '))
+height = float(input('Please Enter the Height of a Rectangle: '))
+
+# calculate the area
+Area = width * height
+
+
+
+print("\n Area of a Rectangle is: %.2f" %Area)
